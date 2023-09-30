0de2c32395c4f2016c32a417ff3ce3d58f0e64e7
jldevia <jldevia81@gmail.com>
2023-09-30 10:40:22 -0300

    Se agrega notificacion al crear una nueva orden

--- src/service-orders/application/useCases/createServiceOrder.ts
+++ src/service-orders/application/useCases/createServiceOrder.ts
@@ -4,12 +4,14 @@ import { ServiceOrderRequest } from 'src/service-orders/dto/serviceOrderReq.dto'
 import { ServiceOrderFactory } from '../factories/serviceOrderFactory';
 import { OrderStatus } from 'src/service-orders/domain/enums/service-order-enums';
 import { ServiceOrder } from 'src/service-orders/domain/entities/serviceOrder.entity';
+import { PubNubClient } from 'src/service-orders/infrastructure/client/pubnub.client';
 @Injectable()
 export class CreateServiceOrder {
   constructor(
     @Inject('ServiceOrderRepository')
     private serviceOrderRepo: ServiceOrderRepository,
     private serviceOrderFactory: ServiceOrderFactory,
+    private notifier: PubNubClient,
   ) {}
 
   async run(request: ServiceOrderRequest): Promise<number> {
@@ -18,20 +20,30 @@ export class CreateServiceOrder {
       request,
     );
 
-    if (this.isOrderAssigned(newServiceOrder)) {
+    const isOrderAssigned =
+      newServiceOrder.status === OrderStatus.PENDING &&
+      newServiceOrder.getValues().execution?.executor;
+
+    if (isOrderAssigned) {
       const execution = newServiceOrder.getValues().execution;
       if (execution) {
         execution.assignedTime = new Date();
       }
     }
     // Save entity
-    return this.serviceOrderRepo.save(newServiceOrder);
+    return this.serviceOrderRepo.save(newServiceOrder).then((id) => {
+      if (isOrderAssigned) {
+        this.notifyAssignedOrder(newServiceOrder);
+      }
+
+      return id;
+    });
   }
 
-  private isOrderAssigned(newServiceOrder: ServiceOrder) {
-    return (
-      newServiceOrder.status === OrderStatus.PENDING &&
-      newServiceOrder.getValues().execution?.executor
-    );
+  private notifyAssignedOrder(serviceOrder: ServiceOrder) {
+    const payloadNotification = serviceOrder.getPayloadNotification();
+    if (payloadNotification) {
+      this.notifier.sendNotification(payloadNotification);
+    }
   }
 }
